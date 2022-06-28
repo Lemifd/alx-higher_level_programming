1112d404c6a224dd0c014a943b878349e6c61a23
Lemi <lemifedasa0@gmail.com>
2022-06-28 06:18:10 -0400

    updating with pycodestye

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,12 +3,11 @@ import random
 number = random.randint(-10000, 10000)
 number = str(number)
 size = len(str(number))
-last_digit = int(number[size-1])
-number=int(number)
-if last_digit > 5:
-    print(f"Last digit of {number:d} is {last_digit} and is greater than 5")
+l_d = int(number[size-1])
+number = int(number)
+if l_d > 5:
+    print(f"Last digit of {number:d} is {l_d} and is greater than 5")
 elif last_digit < 6 and last_digit != 0:
-    print(f"Last digit of {number:d} is {last_digit} and is less than 6 and not 0")
+    print(f"Last digit of {number:d} is {l_d} and is less than 6 and not 0")
 else:
-    print(f"Last digit of {number:d} is {last_digit} and is 0")
-
+    print(f"Last digit of {number:d} is {l_d} and is 0")
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if i < 99:
-        print(f"{i:0=2d}",end=", ")
-    else: 
-        print(f"{i}",end="\n ") 
-
+        print(f"{i:0=2d}", end=", ")
+    else:
+        print(f"{i}", end="\n ")
