e23058d00fccaa0b52efd23ecedfb571ac171314
Lemi <lemifedasa0@gmail.com>
2022-06-28 09:02:40 -0400

    updated 9-print_last_digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number > 0:
+        print(number % 10,end="")
         return (number % 10)
     elif number < 0:
+        print((number % -10)*-1,end="")
         return (number % -10)*-1
     else:
+        print('0',end="")
         return 0
-
