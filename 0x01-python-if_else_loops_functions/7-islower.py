43dfd3f2242487b1d06c405c64e6b8abfb658637
Lemi <lemifedasa0@gmail.com>
2022-06-28 07:56:14 -0400

    updated 7-islower

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def islower(c):
+    if ord(c) >= 97 and ord(c)<=122:
+        return True
+    else:
+        return False
+
+while True:
+    c=input("enter")
+    print(islower(c))
