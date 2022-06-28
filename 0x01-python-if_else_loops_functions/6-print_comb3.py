f917b6f1aaace86177b89ca931e121eedc22a2a1
Lemi <lemifedasa0@gmail.com>
2022-06-28 07:49:19 -0400

    updated 6-print_comb3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(10):
     for j in range(10):
         if j > i and i < 8:
-            print("{:d}{:d}, ".format(i,j), end="")
+            print("{:d}{:d}, ".format(i, j), end="")
         elif j > i and i >= 8:
-            print("{:d}{:d}".format(i,j))
+            print("{:d}{:d}".format(i, j))
