6ab05dcddf4bf50361e22555145eea9a3bb16865
Lemi <lemifedasa0@gmail.com>
2022-06-30 06:07:34 -0400

    updated 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div 
-print("{:d} + {:d} = {:d}".format(10, 5, add(10, 5)))
-print("{:d} - {:d} = {:d}".format(10, 5, sub(10, 5)))
-print("{:d} * {:d} = {:d}".format(10, 5, mul(10, 5)))
-print("{:d} / {:d} = {:d}".format(10, 5, div(10, 5)))
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    print("{:d} + {:d} = {:d}".format(10, 5, add(10, 5)))
+    print("{:d} - {:d} = {:d}".format(10, 5, sub(10, 5)))
+    print("{:d} * {:d} = {:d}".format(10, 5, mul(10, 5)))
+    print("{:d} / {:d} = {:d}".format(10, 5, div(10, 5)))
