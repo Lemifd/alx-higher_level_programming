e19a91c35384c75439ae5ead27a7ce0703f75e33
Lemi <lemifedasa0@gmail.com>
2022-06-30 03:38:48 -0400

    1-calculation

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div 
+print("{:d} + {:d} = {:d}".format(10, 5, add(10, 5)))
+print("{:d} - {:d} = {:d}".format(10, 5, sub(10, 5)))
+print("{:d} * {:d} = {:d}".format(10, 5, mul(10, 5)))
+print("{:d} / {:d} = {:d}".format(10, 5, div(10, 5)))
